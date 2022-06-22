266a10abbc22b58bcc0aa9077fa1bb37d249c1e1
Miguel <marevaloa1400@alumno.ipn.mx>
2022-06-22 00:48:33 -0500

    Article_details.html and views update

---
+++ WebBlog/blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import ArticleDetailView, HomeView
+
+urlpatterns = [ 
+
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>',ArticleDetailView.as_view(),name='article-detail'),
+]
