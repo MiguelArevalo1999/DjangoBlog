212bbc3b580801cce3bceb48299da9eac4153bff
Miguel <marevaloa1400@alumno.ipn.mx>
2022-06-22 23:00:15 -0500

    Update and delete post added

--- WebBlog/blog/urls.py
+++ WebBlog/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from .views import ArticleDetailView, HomeView, AddPostView
+from .views import ArticleDetailView, DeletePostView, HomeView, AddPostView, UpdatePostView, UpdateView
 
 urlpatterns = [ 
 
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>',ArticleDetailView.as_view(),name='article-detail'),
     path('add_post/', AddPostView.as_view(),name = 'add_post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name = 'update_post' ),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name = 'delete_post' ),
+
 ]
--- WebBlog/blog/views.py
+++ WebBlog/blog/views.py
@@ -1,8 +1,9 @@
 import re
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.urls import  reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-from .forms import PostForm
+from .forms import EditForm, PostForm
 
 # def home(request):
 #     return render(request, 'home.html', {})
@@ -10,6 +11,7 @@ from .forms import PostForm
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    # ordering = ['-id']
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -21,3 +23,14 @@ class AddPostView(CreateView):
     template_name = 'add_post.html'
     #fields = '__all__'
     #fields = ('title','body')
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    #fields = ['title','title_tag','body']
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
