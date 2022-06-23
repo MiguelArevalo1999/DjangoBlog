4f1f0eb9ec625cf5ebcd342f72b72f79d3b47b28
Miguel <marevaloa1400@alumno.ipn.mx>
2022-06-23 16:15:03 -0500

    Category post added, categories in nav bar, add category

--- WebBlog/blog/models.py
+++ WebBlog/blog/models.py
@@ -1,8 +1,18 @@
 from tkinter import CASCADE
+from unicodedata import name
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -10,6 +20,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=255, default='coding')
 
     def __str__(self):
         return self.title + '|' + str(self.author)
--- WebBlog/blog/views.py
+++ WebBlog/blog/views.py
@@ -1,8 +1,9 @@
 import re
+from unicodedata import category
 from django.shortcuts import render
 from django.urls import  reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Category, Post
 from .forms import EditForm, PostForm
 
 # def home(request):
@@ -13,15 +14,41 @@ class HomeView(ListView):
     template_name = 'home.html'
     ordering = ['-post_date']
 
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(HomeView,self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
+
+def CategoryListView(request):
+    cat_menu_list = Category.objects.all()
+    return render(request, 'category_list.html', {'cat_menu_list':cat_menu_list})
+
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category=cats.replace('-',' '))
+    return render(request, 'categories.html', {'cats':cats.title().replace('-',' '),'category_posts':category_posts})
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Category.objects.all()
+        context = super(ArticleDetailView,self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
 
 class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
     #fields = '__all__'
+    #fields = ('title','body')
+
+class AddCategoryView(CreateView):
+    model = Category
+    template_name = 'add_category.html'
+    fields = '__all__'
     #fields = ('title','body')
 
 class UpdatePostView(UpdateView):
