961059a35a1577718611f7f9922e3a75604fa3cd
Miguel <marevaloa1400@alumno.ipn.mx>
2022-06-24 12:18:43 -0500

    Add snippet and edit profile section

--- WebBlog/signup/urls.py
+++ WebBlog/signup/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import UserRegisterView
+from .views import UserEditView, UserRegisterView
 
 urlpatterns = [ 
    
    path('register/',UserRegisterView.as_view(),name = 'register'),
+   path('edit_profile/',UserEditView.as_view(),name = 'edit_profile'),
 
    ]
